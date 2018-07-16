a49328a273b8b162c252bf89efdeb5a2561f706d
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-07-16 15:26:29 -0700

    updates

--- jenkins_prep.py
+++ jenkins_prep.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import subprocess
+
 # set the initial admin password (doesn't change until jenkins is setup)
 #f = open('/root/.jenkins/secrets/initialAdminPassword')
 #for line in f:
@@ -7,7 +9,7 @@
 
 
 # setup the suggested and desired plugins list
-f = open('/tmp/docker-jenkins/suggested_plugins.txt', 'r')
+f = open('./plugins.txt', 'r')
 suggested_plugins = []
 for line in f:
   stripped = line.strip()
@@ -16,6 +18,6 @@ for line in f:
 
 i = 0
 while i < len(suggested_plugins):
-  print("installing {0}".format(suggested_plugins[i]))
+  PLUGIN = suggested_plugins[i]
+  subprocess.run(["java", "-jar", "/var/jenkins_home/war/WEB-INF/jenkins-cli.jar", "-s", "http://127.0.0.1:8080/", "-auth", "admin:admin", "install-plugin", PLUGIN])
   i += 1
-  #java -jar WEB-INF/jenkins-cli.jar -s http://127.0.0.1:8080/ -auth admin:{0} install-plugin CCM
