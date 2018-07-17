8d22ead2a78a8ff32100c251d3e3ea2f76082a08
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-07-17 11:53:17 -0700

    testing

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-import http.client
+import urllib.request
 import subprocess
 import time
 
@@ -20,8 +20,6 @@ while i < len(suggested_plugins):
   PLUGIN = suggested_plugins[i]
   subprocess.run(["java", "-jar", "/var/jenkins_home/war/WEB-INF/jenkins-cli.jar", "-s", "http://127.0.0.1:8080/", "-auth", "admin:admin", "install-plugin", PLUGIN])
   i += 1
-
-jenkins_start.wait()
 
 status = http.client.HTTPConnection('http://127.0.0.1:8080/restart')
 print("status: {}".format(status))
