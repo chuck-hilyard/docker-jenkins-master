a69c6de6040665866d13b485f00bbed26d2cb747
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-07-20 10:24:37 -0700

    testing

--- init.py
+++ init.py
@@ -46,7 +46,7 @@ for repo in f:
     BRANCH = response.text
   else:
     BRANCH = "master"
-    REPO_CONFIG_FILE_DIR = "/var/jenkins_home/jobs/{}/config.xml".format(REPO_NAME)
+    CONFIG_FILE_DIR = "/var/jenkins_home/jobs/{}/config.xml".format(REPO_NAME)
   try:
     subprocess.run(["git", "clone", REPO_URL, TARGET_FOLDER, "--branch", BRANCH, "--depth", "1"])
   except:
@@ -58,12 +58,12 @@ for repo in f:
     formatted_template = template_repo_config_string.format(REPO_URL=REPO_URL, BRANCH=BRANCH)
     print("****************************** writing config.xml *********************")
     print("formatted_template: ", formatted_template)
-    print("CONFIG FILE DIR: ", REPO_CONFIG_FILE_DIR)
-    repo_config_xml = open(REPO_CONFIG_FILE_DIR, 'w')
+    print("CONFIG FILE DIR: ", CONFIG_FILE_DIR)
+    repo_config_xml = open(CONFIG_FILE_DIR, 'w')
     repo_config_xml.write(formatted_template)
     repo_config_xml.close()
   except FileNotFoundError as e:
-    print("file copy to {} failed".format(REPO_CONFIG_FILE_DIR))
+    print("file copy to {} failed".format(CONFIG_FILE_DIR))
 
 # after all the changes, hit restart
 subprocess.run(["curl", "-X", "POST", "-u", "admin:admin", "http://127.0.0.1:8080/safeRestart"])
