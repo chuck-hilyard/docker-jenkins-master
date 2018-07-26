e0737ab0670e061fdc49c1a01fa0626cd9dde926
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-07-26 14:35:53 -0700

    testing

--- init.py
+++ init.py
@@ -79,7 +79,7 @@ for repo in f:
 try:
   private_key_file = open('/var/jenkins_home/.ssh/id_rsa', 'r')
   PRIVATE_KEY_TMP = private_key_file.read()
-  PRIVATE_KEY = PRIVATE_KEY_TMP.strip()
+  PRIVATE_KEY = PRIVATE_KEY_TMP.strip('\n')
   print("********************************************************************************")
   print("PRIVATE KEY is ", type(PRIVATE_KEY))
   print("PRIVATE KEY: {}".format(PRIVATE_KEY))
