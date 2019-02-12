088ad27f830eb92047d132cf60aac82dba118ade
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-02-12 14:36:45 -0800

    fixed permission problem under jenkins home

--- Docker-entrypoint.sh
+++ Docker-entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 git clone https://github.com/chuck-hilyard/jenkins-rl-bin.git /var/jenkins_home/jenkins-rl-bin
-chown -R jenkins:jenkins /var/jenkins_home
+chown -R jenkins:jenkins /var/jenkins_home/jenkins-rl-bin
 
 exec "$@"
